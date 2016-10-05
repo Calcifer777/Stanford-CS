7f8135cde3dffbb9268fb7c38fb89ca6f1196475
Calcifer777 <m.filip@hotmail.it>
2016-10-05 12:23:44 +0200

    Update RandomColorLabel.java

--- The-Art-and-Science-of-Java/Chapter-9/RandomColorLabel.java
+++ The-Art-and-Science-of-Java/Chapter-9/RandomColorLabel.java
@@ -1,127 +1,163 @@
 
+
+/* Displays a label containing the name of a random color. The color of the label is set Not
+ * to match the label color. Every second a new label (and a new color) are generated */
+
+
 import acm.program.*;
 import acm.util.RandomGenerator;
 import acm.graphics.*;
 import java.awt.*;
+import java.lang.reflect.Field;
+import java.util.StringTokenizer;
 
-public class RandomColorLabel extends GraphicsProgram {
+public class RandomColorLabelV2 extends GraphicsProgram {
 	
 	public void run() {
 		
+		/* Setting the applet window size to the preferred dimensions*/
+		
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-		/* Creating a new canvas with the dimensions of the image */
+		/* Creating a new canvas */
 		
 		GCanvas gc = new GCanvas();
 		gc.setVisible(true);
 		
 		gc.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		
+		GLabel label = new GLabel("");
+		
+		label.setFont("SansSerifs-bold-40");
+		
 		while (true) {
 			
 			/* Creating the label */
 			
-			String stringContent="";
+			String stringContent= newColor(LIST);
 			
-			/* Calling rgen to set the content of the label */
+			label.setLabel(stringContent);
 			
-			int roll =  rgen.nextInt(1,7);
-			
-			switch (roll) {
-			case 1: 
-				stringContent = "RED";
-				break;
-			case 2: 
-				stringContent = "ORANGE";
-				break;
-			case 3: 
-				stringContent = "YELLOW";
-				break;
-			case 4:  
-				stringContent = "GREEN";
-				break;
-			case 5:  
-				stringContent = "CYAN";
-				break;
-			case 6: 
-				stringContent = "BLUE";
-				break;
-			case 7:  
-				stringContent = "MAGENTA";
-				break;
-			default: 
-				stringContent = "BLACK";
-				break;
-			}
-
-			GLabel label = new GLabel(stringContent);
-			label.setFont("SansSerifs-bold-30");
 			/* Coloring the label */
 			
 			boolean ok = false;
-			Color labelColor = Color.BLACK;
 			
 			while (!ok) {
 				
-				int colorRoll = rgen.nextInt(1,7);
+				String labelColor=newColor(LIST);
 				
-				if (colorRoll!=roll) {
-					
-					switch (colorRoll) {
-					case 1: 
-						labelColor = Color.RED;
-						break;
-					case 2: 
-						labelColor = Color.ORANGE;
-						break;
-					case 3: 
-						labelColor = Color.YELLOW;
-						break;
-					case 4: 
-						labelColor = Color.GREEN; 
-						break;
-					case 5: 
-						labelColor = Color.CYAN; 
-						break;
-					case 6: 
-						labelColor = Color.BLUE;
-						break;
-					case 7: 
-						labelColor = Color.MAGENTA; 
-						break;
-					default: 
-						labelColor = Color.BLACK;
-						break;
-					}
-
-					label.setColor(labelColor);
+				if (!stringContent.equals(labelColor)) { 	// Color the label only with a color that does not
+										// match the string content
+				
+					label.setColor(stringToColor(labelColor));
 					
 					ok=true;
 					
 				}
 				
 			}
-						
-			GPoint labelLocation = new GPoint(rgen.nextDouble(0,gc.getWidth()-label.getWidth() ),
-					rgen.nextDouble(label.getAscent(),  gc.getHeight()-label.getDescent()));
+				
+			
+			/* Sets the label at the center of the canvas */ 
+			
+			GPoint labelLocation = new GPoint( (gc.getWidth()-label.getWidth() )/2,
+					( gc.getHeight()-label.getDescent())/2 );
 					
 			add(label,labelLocation);
 			
 			pause(PAUSE_TIME);
-			
-			remove(label);
-			
+						
 		}
 		
 		
 	}
 
+	/** Generates a new color from a list of colors.
+	 * @param colorList is a string. Colors may be in integer format ( 200, 150, 170) or just the names of the colors
+	 * (e.g. red, black, etc) */
 	
-	RandomGenerator rgen = new RandomGenerator();
+	private String newColor(String colorList) {
+		
+		StringTokenizer tokenizer = new StringTokenizer(colorList, " ", false);
+		
+		/* Counts the colors in the list */
+		
+		int colorsNumber = tokenizer.countTokens();
+		
+		/* Randmly selects a color in the list, stored as a token index*/
+		
+		int n = rgen.nextInt(1, colorsNumber);		
+		
+		int i=1;
+		
+		while ( i< n) {
+			tokenizer.nextToken();
+			i++;
+		}
+		
+		/* Returns the nth token/color in the list as a string */
+		
+		return tokenizer.nextToken();
+		
+	}
 	
-	/* Constants */
 	
-	private static final int WINDOW_WIDTH = 800;
-	private static final int WINDOW_HEIGHT = 450;
-	private static final int PAUSE_TIME = 1000;
+	/** Converts a given string into a color.
+	  * @param value The string containing the color; either a name or an integer.
+	  * @return The color. Returns black as a default color.
+	  */
+	
+	  private static Color stringToColor(final String value) {
+		  
+	    
+		  if (value == null) {
+	      
+			  return Color.black;
+	
+		  }
+	    
+		  try {
+	      
+			  // get color by hex or octal value
+	      
+			  return Color.decode(value);
+	
+		  } catch (NumberFormatException nfe) {
+	      
+			  // if we can't decode lets try to get it by name
+	      
+			  try {
+	        
+				  // try to get a color by name using reflection
+	        
+				  final Field f = Color.class.getField(value);
+
+				  return (Color) f.get(null);
+
+			  } catch (Exception ce) {
+	       
+				  // if we can't get any color return black
+	        
+				  return Color.black;
+	     
+			  }
+	  
+		  }
+		  
+	  }
+	  
+	  
+	  /* Instance of the randomgenerator class */
+	
+	  RandomGenerator rgen = new RandomGenerator();
+	
+
+	  /* Constants */
+
+	  private static final int WINDOW_WIDTH = 800;
+	  private static final int WINDOW_HEIGHT = 450;
+	  private static final int PAUSE_TIME = 500;
+	
+	private static final String LIST = "RED ORANGE YELLOW GREEN CYAN BLUE MAGENTA";
+
 }
