46f1b7b017eadc7e3a122d3368e05286f91fa899
Calcifer777 <m.filip@hotmail.it>
2016-10-13 00:40:00 +0200

    Update and rename RotateLeft.java to RotateImage.java

--- The-Art-and-Science-of-Java/Chapter-11/RotateImage.java
+++ The-Art-and-Science-of-Java/Chapter-11/RotateImage.java
@@ -2,24 +2,25 @@
 import acm.program.*;
 import acm.graphics.*;
 
-/* Application of the flipHorizontal method. 
+/* Example of two methods that rotate a given image and display the result
  * The flipHorizontal method flips a GImage horizontally. */
 
-public class RotateLeft extends GraphicsProgram {
+public class RotateImage extends GraphicsProgram {
 	
 	public void run() {
 		
+		/* Loads the image */
 		GImage quokka = new GImage("quokka.jpg");
 		
+		/* Records the height and width of the image and sets the window applet dimensions
+		 * accordingly */
 		int imageWidth = (int) quokka.getWidth();
 		int imageHeight = (int) quokka.getHeight();
-		
 		setSize( (imageWidth+OFFSET)*3, (imageWidth>imageHeight)?imageWidth:imageHeight+OFFSET*2);
 		
+		/* Adds the original image, as well as the right-rotated and left-rotated version */ 
 		add(quokka, OFFSET, OFFSET);
-		
 		add(rotateRight(quokka), OFFSET*2+imageWidth, OFFSET );
-		
 		add(rotateLeft(quokka), OFFSET*3+imageWidth+imageHeight, OFFSET );
 		
 		
@@ -27,16 +28,16 @@ public class RotateLeft extends GraphicsProgram {
 	
 	private GImage rotateRight(GImage image) {
 		
+		/* Creates a bidimensional int array from the image */
 		int[][] array = image.getPixelArray();
 		
- 
+		/* Creates a bidimensional int array for the rotated image;
+		 * width and height are reversed compared to the original image. */
 		int imageHeight =  array.length;
 		int imageWidth =  array[0].length;
-		
-		print("Image size: h "+ imageHeight+"; w "+ imageWidth);
-		
 		int[][] rotatedArray = new int[imageWidth][imageHeight];
 		
+		/* Setting the values for the rotated image */
 		for (int row = 0 ; row < imageHeight; row++) {
 			for (int col = 0; col < imageWidth; col++) {
  
